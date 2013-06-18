d1a66215dda803fbd1f8fe96e25188e19d1b0cf4
bedhed <clement@seizam.com>
2013-06-18 12:56:19 +0200

    Licensing i18n

--- LocalSettings.i18n.php
+++ LocalSettings.i18n.php
@@ -7,11 +7,38 @@ $messages['en'] = array(
 	'protect-level-acarya' => 'Allow only acaryas',
 	'protect-level-lama' => 'Allow only lamas',
 	'protect-level-vajracarya' => 'Allow only vajracaryas',
+	
+	/* Licensing */
+    'license-no' => 'undefined license',
+    'license-text' => 'This page and its content are licensed under a $1.',
+    'no-license' => 'No license has been selected for this page and its content. All rights are assumed to be reserved.',
+    'copyright-text' => 'This page and its content are copyrighted. All rights reserved.',
+    
+    'cc-by-30' => 'Creative Commons Attribution 3.0 License',
+    'cc-by-sa-30' => 'Creative Commons Attribution-ShareAlike 3.0 License',
+    'cc-by-nd-30' => 'Creative Commons Attribution-NoDerivs 3.0 License',
+    'cc-by-nc-30' => 'Creative Commons Attribution-NonCommercial 3.0 License',
+    'cc-by-nc-sa-30' => 'Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License',
+    'cc-by-nc-nd-30' => 'Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License',
 );
 
 $messages['fr'] = array(
 	'protect-level-adherent' => 'Autoriser uniquement les pratiquants',
 	'protect-level-acarya' => 'Autoriser uniquement les acaryas',
 	'protect-level-lama' => 'Autoriser uniquement les lamas',
-	'protect-level-vajracarya' => 'Autoriser uniquement les vajracaryas'
+	'protect-level-vajracarya' => 'Autoriser uniquement les vajracaryas',
+	
+	/* Licensing */
+    'undefined-license' => 'licence indéfinie',
+    'no-license' => 'Aucune licence n\'a été sélectionnée pour cette page et sont contenu. Tous les droits sont considérés réservés.',
+    'license-text' => 'Cette page et son contenu sont soumis à la $1.',
+    'copyright-text' => 'Cette page et son contenu sont soumis à copyright. Tous droits réservés.',
+    
+    'cc-by-30' => 'Licence Creative Commons Attribution 3.0',
+    'cc-by-sa-30' => 'Licence Creative Commons Attribution - Partage à l\'Identique 3.0',
+    'cc-by-nd-30' => 'Licence Creative Commons Attribution - Pas de Modification 3.0',
+    'cc-by-nc-30' => 'Licence Creative Commons Attribution - Pas d\'Utilisation Commerciale 3.0',
+    'cc-by-nc-sa-30' => 'Licence Creative Commons Attribution - Pas d\'Utilisation Commerciale - Partage à l\'Identique 3.0',
+    'cc-by-nc-nd-30' => 'Licence Creative Commons Attribution - Pas d\'Utilisation Commerciale - Pas de Modification 3.0',
+    
 );
